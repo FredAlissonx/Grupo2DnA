a3def08a016988a0c881c7841455c8707e36ac60
Fred Alisson <fredalissonsousa@gmail.com>
2024-04-07 22:30:48 -0300

    feat: Understading the dataset

---
+++ script.sql
@@ -0,0 +1,246 @@
+CREATE DATABASE IF NOT EXISTS churn_analysis;
+USE churn_analysis;
+
+SELECT * FROM churn;
+
+/*
+-----||| Understanding the Dataset |||-----
+*/
+-- Some info about the dataset
+DESCRIBE churn;
+
+-- Number of columns
+SELECT
+	COUNT(column_name) AS numberOfColumns
+FROM information_schema.columns
+WHERE
+	table_schema = 'churn_analysis'
+    AND
+    table_name = 'churn';
+
+-- Number of rows
+SELECT
+	COUNT(*) AS numberOfRows
+FROM churn;
+
+-- Column name and description
+SELECT
+	column_name AS columnName,
+	CASE 
+		WHEN column_name = 'customerid' THEN 'Unique identifier of each customer'
+		WHEN column_name = 'creditscore' THEN 'A customer ability to pay their debts and meet their financial obligations'
+		WHEN column_name = 'geography' THEN 'Country the customer is in'
+		WHEN column_name = 'gender' THEN 'Customer gender (Male/Female)'
+		WHEN column_name = 'age' THEN 'Describe the age of the customers'
+		WHEN column_name = 'tenure' THEN 'Period of time (years) that a customer maintains an active relationship with a bank or financial institution'
+		WHEN column_name = 'balance' THEN 'Amount of money that is available or that remains in a bank account after all transactions have been considered'
+		WHEN column_name = 'numofproducts' THEN 'Number of products/services that a customer has or had'
+		WHEN column_name = 'hascrcard' THEN '1 if has credit card and 0 if not'
+        WHEN column_name = 'isactivemember' THEN '1 if is active member and 0 if not'
+        WHEN column_name = 'estimatedsalary' THEN 'Estimated salary in dollar per month'
+        WHEN column_name = 'exited' THEN '1 if exited and 0 if not'
+	END as description
+FROM information_schema.columns
+WHERE 
+	table_schema = 'churn_analysis'
+    AND
+    table_name = 'churn';
+
+-- Checking missing values
+SELECT
+	COUNT(*) AS missingValues
+FROM churn
+WHERE NULL IN (customerid, creditscore, geography, gender, age, tenure, balance, numofproducts, hascrcard, isactivemember, estimatedsalary, exited);
+
+
+/*--| Show categorical values |--*/
+
+-- Credit score
+SELECT
+	creditScore,
+    COUNT(customerid) AS quantityOfCustomers
+FROM churn
+GROUP BY creditscore;
+
+-- Geography
+SELECT
+	geography,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY geography;
+
+-- Gender
+SELECT
+	gender,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY gender;
+
+-- Age
+SELECT
+	age,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY age;
+
+-- Tenure
+SELECT
+	tenure,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY tenure;
+
+-- Number of products
+SELECT
+	numofproducts,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY numofproducts;
+
+-- Has credit card
+SELECT 
+    IF(hascrcard = 1, 'Yes', 'No') AS hascrCard,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY hascrCard;
+
+-- Is active member
+SELECT 
+    IF(isactivemember = 1, 'Yes', 'No') AS isactivemember,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY isactivemember;
+
+-- Exited
+SELECT 
+    IF(exited = 1, 'Yes', 'No') AS exited,
+    COUNT(customerId) AS quantityOfCustomers
+FROM churn
+GROUP BY exited;
+
+
+
+/*
+--| Data distribution |--*/	
+
+/*-- Data range --*/
+-- Credit score
+SELECT
+	MIN(creditscore) AS minCreditScore,
+    MAX(creditscore) AS maxCreditScore,
+    ROUND(AVG(creditscore), 2) AS averageCreditScore
+FROM churn;
+
+-- Age
+SELECT
+	MIN(age) AS minAge,
+    MAX(age) AS maxAge,
+    FLOOR(AVG(age)) As averageAge
+FROM churn;
+
+-- Tenure
+SELECT
+	MIN(tenure) AS minTenure,
+    MAX(tenure) AS maxTenure,
+    FLOOR(AVG(tenure)) As averageTenure
+FROM churn;
+
+-- Balance
+SELECT
+	MIN(balance) AS minBalance,
+    MAX(balance) AS maxBalance,
+    ROUND(AVG(balance), 2) As averageBalance
+FROM churn;
+
+-- Number of products
+SELECT
+	MIN(numofproducts) AS minNumOfProducts,
+    MAX(numofproducts) AS maxNumOfProducts,
+    CEIL(AVG(numofproducts)) As averageNumberOfProducts
+FROM churn;
+
+-- Estimated Salary per year
+SELECT
+	MIN(estimatedsalary) AS minEstimatedSalary,
+    MAX(estimatedsalary) AS maxEstimatedSalary,
+    ROUND(AVG(estimatedsalary), 2) AS averageEstimatedSalary
+FROM churn;
+
+
+/*-- Histograms and Frequency Distributions --*/
+
+-- Credit score
+SELECT
+    FLOOR(creditScore / 10) * 10 AS creditScoreRange,
+    COUNT(*) AS frequency
+FROM
+    churn
+GROUP BY creditScoreRange;
+
+-- Age
+SELECT
+    FLOOR(age / 10) * 10 AS ageRange,
+    COUNT(*) AS frequency
+FROM
+    churn
+GROUP BY age_range;
+
+-- Tenure
+SELECT
+    FLOOR(tenure / 2) * 2 AS tenureRange,
+    COUNT(*) AS frequency
+FROM
+    churn
+GROUP BY tenureRange;
+
+-- Balance
+SELECT
+    FLOOR(balance / 2500) * 2500 AS balanceRange,
+    COUNT(*) AS frequency
+FROM
+    churn
+GROUP BY balanceRange;
+
+-- Number of products
+SELECT
+    FLOOR(numOfProducts / 2) * 2 AS numberOfProductsRange,
+    COUNT(*) AS frequency
+FROM
+    churn
+GROUP BY numberOfProductsRange;
+
+-- Estimated salary
+SELECT
+    FLOOR(estimatedSalary / 20000) * 20000 AS estimatedSalaryRange,
+    COUNT(*) AS frequency
+FROM
+    churn
+GROUP BY estimatedSalaryRange
+ORDER by estimatedSalaryRange;
+
+/*
+-----||| END - Understanding the Dataset |||-----
+*/
+
+-- ------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
